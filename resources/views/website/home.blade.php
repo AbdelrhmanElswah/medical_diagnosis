a780474c230ef13260dcd799f233934f3ecdae08
Ahmed Abouemira <a.aboemira1@gmail.com>
2024-04-24 03:42:17 +0200

    Update home.blade.php

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Doctorna</title>
-    <link rel="stylesheet" href="{{ asset('website/css/style.css') }}" />
     <link  href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css')}}" rel="stylesheet" />
     <link href="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css')}}" rel="stylesheet" />
     <link href="{{ asset('https://getbootstrap.com/docs/5.3/assets/css/docs.css')}}" rel="stylesheet" />
     <script src="{{ asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('website/css/style.css') }}" />
 
 </head>
 <body>
