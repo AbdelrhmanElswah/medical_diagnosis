88d2778863b67226e8839c106f2751dfd466c388
omar walid <114596335+omarwalid66@users.noreply.github.com>
2024-06-26 15:41:37 +0300

    upload button and text color and footer background

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,57 +1,54 @@
 
     <footer class="text-center text-lg-start text-light">
-      <section
-        class="d-flex justify-content-between p-4 text-white"
-      ></section>
-      <section class="">
-        <div class="container text-center text-md-start mt-5 text-light">
+      <section class="footer-background">
+        <div class="container text-center text-md-start mt-5 text-dark">
           <div class="row mt-3">
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 pt-5">
               <h6 class="text-uppercase fw-bold">Support</h6>
               <hr
                 class="mb-4 mt-0 d-inline-block mx-auto"
                 style="width: 60px; background-color: white; height: 4px"
               />
               <p id="ff">
-                <a href="#!" class="text-light" style="text-decoration: none"
+                <a href="#!" class="text-dark" style="text-decoration: none"
                   >FAQS</a
                 >
               </p id="ff">
               <p id="ff">
-                <a href="#!" class="text-light" style="text-decoration: none"
+                <a href="#!" class="text-dark" style="text-decoration: none"
                   >Privcy Policy</a
                 >
               </p>
               <p id="ff">
-                <a href="#!" class="text-light" style="text-decoration: none"
+                <a href="#!" class="text-dark" style="text-decoration: none"
                   >Terms & Conditions</a
                 >
               </p>
             </div>
-            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4 pt-5">
               <h6 class="text-uppercase fw-bold">Services</h6>
               <hr
                 class="mb-4 mt-0 d-inline-block mx-auto"
                 style="width: 60px; background-color: white; height: 4px"
               />
               <p id="ff">
-                <a href="upload.html" class="text-light" style="text-decoration: none"
+                <a href="upload.html" class="text-dark" style="text-decoration: none"
                   >Brain Tumor Detection
                 </a>
               </p>
               <p id="ff">
-                <a href="upload.html" class="text-light" style="text-decoration: none"
+                <a href="upload.html" class="text-dark" style="text-decoration: none"
                   >Chest Disease Identification</a
                 >
               </p>
               <p id="ff">
-                <a href="upload.html" class="text-light" style="text-decoration: none"
+                <a href="upload.html" class="text-dark" style="text-decoration: none"
                   >Eye Disease Diagnosis:
                 </a>
               </p>
             </div>
 
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 pt-5">
               <h6 class="text-uppercase fw-bold">Contact</h6>
               <hr
                 class="mb-4 mt-0 d-inline-block mx-auto"
@@ -61,7 +58,7 @@
               <p><i class="fas fa-envelope mr-3"></i> Doctorna@gmail.com</p>
               <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
             </div>
-            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4">
+            <div class="col-md-3 col-lg-2 col-xl-3 mx-auto mb-4 pt-5">
               <h6 class="text-uppercase fw-bold">Follow Us</h6>
               <hr
                 class="mb-4 mt-0 d-inline-block mx-auto"
@@ -69,13 +66,13 @@
               />
               <p >
                 <a href="#" 
-                  ><i class="fab fa-facebook-f text-light pe-3"></i
+                  ><i class="fab fa-facebook-f text-dark pe-3"></i
                 ></a>
                 <a href="#"
-                  ><i class="fab fa-instagram text-light pe-3"></i
+                  ><i class="fab fa-instagram text-dark pe-3"></i
                 ></a>
                 <a href="#"
-                  ><i class="fab fa-linkedin-in text-light pe-3"></i
+                  ><i class="fab fa-linkedin-in text-dark pe-3"></i
                 ></a>
               </p>
             </div>
