5126638e500a8d20dcf8d0b788d2f6beaaf15bb7
omar walid <114596335+omarwalid66@users.noreply.github.com>
2024-06-29 03:58:47 +0300

    footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,49 +1,49 @@
 <footer class="page-footer shadow" style="background-color: white">
-  <div class="d-flex flex-column mx-auto pt-5" id="om" style="width: 80%">
+  <div class="d-flex flex-column mx-auto pt-5" style="width: 80%">
     <div class="d-flex flex-wrap justify-content-between">
       <div>
         <a href="/" class="d-flex align-items-center p-0 text-dark" style="text-decoration: none">
           <img alt="logo" src="{{ asset('website/img/2536.jpg') }}" width="50px" />
           <span class="ms-3 h5 font-weight-bold">Doctorna</span>
         </a>
-        <p class="my-3" style="width: 250px">
+        <p class="my-3" style="width: 250px; text-align: left;">
           Doctorna harnesses the power of advanced AI models to provide
           accurate and timely medical diagnoses. Trust Doctorna to bring
           the future of healthcare to your family and friends.
         </p>
-        <span class="mt-4">
-          <button class="btn btn-dark btn-flat p-2" style="margin-bottom: 30px">
+        <span class="mt-4 d-flex" style="gap: 10px; margin-bottom: 20px;">
+          <button class="btn btn-dark btn-flat p-2">
             <i class="fa-brands fa-facebook"></i>
           </button>
-          <button class="btn btn-dark btn-flat p-2" style="margin-bottom: 30px">
+          <button class="btn btn-dark btn-flat p-2">
             <i class="fa-brands fa-linkedin"></i>
           </button>
-          <button class="btn btn-dark btn-flat p-2" style="margin-bottom: 30px">
+          <button class="btn btn-dark btn-flat p-2">
             <i class="fa-brands fa-instagram"></i>
           </button>
         </span>
       </div>
-      <div>
-        <p class="h5 mb-4 w-75 text-center" style="font-weight: 600; background-color: #acd9d1; border-radius: 8px;">
+      <div class="text-center">
+        <p class="h5 mb-4" style="font-weight: 600; background-color: #acd9d1; border-radius: 8px;">
           Services
         </p>
-        <ul class="p-0 footer-bttn" style="list-style: none; cursor: pointer">
-          <li class="my-2 pt-3">
+        <ul class="p-0 footer-bttn" style="list-style: none; cursor: pointer; text-align: left;">
+          <li class="pt-3 service-item">
             <a class="text-dark" href="/">Brain Tumor Detection</a>
           </li>
-          <li class="my-2 pt-3">
+          <li class="pt-3 service-item">
             <a class="text-dark" href="/">Chest Disease Identification</a>
           </li>
-          <li class="my-2 pt-3">
+          <li class="my-2 pt-3 service-item">
             <a class="text-dark" href="/">Eye Disease Diagnosis</a>
           </li>
         </ul>
       </div>
-      <div>
-        <p class="h5 mb-4 w-auto text-center" style="font-weight: 600; background-color: #acd9d1; border-radius: 8px;">
+      <div class="text-center">
+        <p class="h5 mb-4" style="font-weight: 600; background-color: #acd9d1; border-radius: 8px;">
           Contact Us
         </p>
-        <ul class="p-0" style="list-style: none; cursor: pointer">
+        <ul class="p-0" style="list-style: none; cursor: pointer; text-align: left;">
           <li class="my-2 pt-3">
             <p class="text-dark">
               <i class="fa-solid fa-house"></i> Portsaid, Egypt
@@ -62,3 +62,8 @@
     </div>
   </div>
 </footer>
+
+
+<style>
+
+</style>
